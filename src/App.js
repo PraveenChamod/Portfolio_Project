3e7c0b8525abd8770239d91fc4bc9409ee410489
PraveenChamod <praveenchamod23@gmail.com>
2023-04-17 00:33:34 +0530

    Added Linear Gradient for Buttons.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,49 @@ import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Project from './components/Project/Project';
+import Aos from 'aos';
+import {useEffect} from 'react';
+import ScrollToTop from './Hooks/ScrollToTop';
 
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      disable: 'mobile',
+      duration:1000
+    });
+  }, [])
+  useEffect(() => {
+    const scrollToTop = ()=>{
+      if(window.pageYOffset > 200)
+      {
+        window.scrollTo(
+          {
+            top: 0, 
+            behavior: "smooth",
+          }
+        );
+      }
+    }
+
+  }, [])
+
+  const scrollToTop = ()=>{
+    if(window.pageYOffset > 200)
+    {
+      window.scrollTo(
+        {
+          top: 0, 
+          behavior: "smooth",
+        }
+      );
+    }
+  }
+
+
   return (
     <div className="App">
+      <ScrollToTop/> 
       <Home />
       <AboutMe />
       <Project />
